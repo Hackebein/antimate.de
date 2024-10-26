790fafeee827203ea7f4dfa1cbc6a56c1a4d5693
Lars Olzem <hackebein@gmail.com>
2024-10-26 08:27:04 +0000

    fix: add missing dns record

--- dns/dnsconfig.js
+++ dns/dnsconfig.js
@@ -2,5 +2,7 @@ var REG_OVH = NewRegistrar("ovh");
 var DSP_OVH = NewDnsProvider("ovh");
 
 D("antimate.de", REG_OVH, DnsProvider(DSP_OVH),
+    // TeamSpeak
     SRV("_ts3._udp", 0, 5, 20101, "ts3server01.antimate.de."),
+    A("ts3server01", "5.9.72.186"),
 END);
